f2b219cd1a9143622ddf5c28b9a29e9743eb51e7
Asadullah Ehsan <asadullahsuri131@gmail.com>
2025-03-29 18:55:44 +0500

    core functionality setup

---
+++ URL_Shortener/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace URL_Shortener.Models;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<ShortenedUrl> ShortenedUrls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShortenedUrl>((builder) => {
+            builder
+            .Property(shortenedUrl => shortenedUrl.Code)
+            .HasMaxLength(ShortLinkSettings.Length);
+
+            builder
+            .HasIndex(shortenedUrl => shortenedUrl.Code)
+            .IsUnique();
+        });
+    }
+}
---
+++ URL_Shortener/ShortLinkSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace URL_Shortener;
+
+public static class ShortLinkSettings
+{
+    public const int Length = 7;
+    public const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+}
